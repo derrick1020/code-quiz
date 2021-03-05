8569d8ba8b46174642d721a0249db00623e250ac
derrick helgeson <dkmk1020@gmail.com>
2021-03-04 18:54:49 -0800

    code quiz updated

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # code-quiz
+This is my code Quiz you have 20 seconds to answer 5 questions.
+If you get an answer wrong then you lose 3 seconds on your time.
+Your score goes up the faster you complete the quiz.
+It saves your high score so you can try and beat your time.
+I used some simple html and styled it with css.
+most of the page itself is java code.
+When you click the start button the quiz begins and the timer starts counting down.
+when you finish you will be asked to add your initals and save your score.
+you can also delete your high score and start over.
---
+++ script.js
@@ -0,0 +1,207 @@
+let secondsLeft = 30;
+
+let timer = document.getElementById("timer");
+
+let scoresDiv = document.getElementById("scores-div");
+
+let buttonsDiv = document.getElementById("buttons")
+
+let viewScoresBtn = document.getElementById("view-scores")
+
+//start button
+let startButton = document.getElementById("start-button");
+startButton.addEventListener("click", setTime);
+
+
+var questionDiv = document.getElementById("question-div");
+
+let results = document.getElementById("results");
+
+var choices = document.getElementById("choices");
+
+
+let emptyArray = [];
+
+let storedArray = JSON.parse(window.localStorage.getItem("highScores"));
+var questionCount = 0;
+
+let score = 0
+
+function setTime() {
+  displayQuestions();
+  let timerInterval = setInterval(function() {
+    secondsLeft--;
+    timer.textContent = "";
+    timer.textContent = "Time: " + secondsLeft;
+    if (secondsLeft <= 0 || questionCount === questions.length) {
+      clearInterval(timerInterval);
+      captureUserScore();
+    } 
+  }, 1000);
+}
+
+//load questions
+function displayQuestions() {
+  removeEls(startButton);
+
+  if (questionCount < questions.length) {
+    questionDiv.innerHTML = questions[questionCount].title;
+    choices.textContent = "";
+
+    for (let i = 0; i < questions[questionCount].multiChoice.length; i++) {
+      let el = document.createElement("button");
+      el.innerText = questions[questionCount].multiChoice[i];
+      el.setAttribute("data-id", i);
+      el.addEventListener("click", function (event) {
+        event.stopPropagation();
+
+        if (el.innerText === questions[questionCount].answer) {
+          score += secondsLeft;
+        } else {
+          score -= 10;
+          secondsLeft = secondsLeft - 3;
+        }
+        
+        questionDiv.innerHTML = "";
+
+        if (questionCount === questions.length) {
+          return;
+        } else {
+          questionCount++;
+          displayQuestions();
+        }
+      });
+      choices.append(el);
+    }
+  }
+}
+
+
+function captureUserScore() {
+  timer.remove();
+  choices.textContent = "";
+
+  let initialsInput = document.createElement("input");
+  let postScoreBtn = document.createElement("input");
+
+  results.innerHTML = `You scored ${score} points! Enter initials: `;
+  initialsInput.setAttribute("type", "text");
+  postScoreBtn.setAttribute("type", "button");
+  postScoreBtn.setAttribute("value", "Post My Score!");
+  postScoreBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    let scoresArray = defineScoresArray(storedArray, emptyArray);
+
+    let initials = initialsInput.value;
+    let userAndScore = {
+      initials: initials,
+      score: score,
+    };
+
+    scoresArray.push(userAndScore);
+    saveScores(scoresArray);
+    displayAllScores();
+    clearScoresBtn();
+    goBackBtn();
+    viewScoresBtn.remove();
+  });
+  results.append(initialsInput);
+  results.append(postScoreBtn);
+}
+
+const saveScores = (array) => {
+  window.localStorage.setItem("highScores", JSON.stringify(array));
+}
+
+const defineScoresArray = (arr1, arr2) => {
+  if(arr1 !== null) {
+    return arr1
+  } else {
+    return arr2
+  }
+}
+
+const removeEls = (...els) => {
+  for (let el of els) el.remove();
+}
+
+function displayAllScores() {
+  removeEls(timer, startButton, results);
+  let scoresArray = defineScoresArray(storedArray, emptyArray);
+
+  scoresArray.forEach(obj => {
+    let initials = obj.initials;
+    let storedScore = obj.score;
+    let resultsP = document.createElement("p");
+    resultsP.innerText = `${initials}: ${storedScore}`;
+    scoresDiv.append(resultsP);
+  });
+}
+
+function viewScores() {
+  viewScoresBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    removeEls(timer, startButton);
+    displayAllScores();
+    removeEls(viewScoresBtn);
+    clearScoresBtn();
+    goBackBtn();
+  });
+}
+
+function clearScoresBtn() {    
+  let clearBtn = document.createElement("input");
+  clearBtn.setAttribute("type", "button");
+  clearBtn.setAttribute("value", "Clear Scores");
+  clearBtn.addEventListener("click", function(event){
+    event.preventDefault();
+    removeEls(scoresDiv);
+    window.localStorage.removeItem("highScores");
+  })
+  scoresDiv.append(clearBtn)
+}
+
+function goBackBtn() {
+  let backBtn = document.createElement("input");
+  backBtn.setAttribute("type", "button");
+  backBtn.setAttribute("value", "Go Back");
+  backBtn.addEventListener("click", function(event){
+    event.preventDefault();
+    window.location.reload();
+  })
+  buttonsDiv.append(backBtn)
+}
+
+
+viewScores();
+var questions = [
+    {
+      title: "What does CSS stand for?",
+      multiChoice: ["cascading style sheet", "central style sheet", "central system storage", "cascading system sheet"],
+      answer: "cascading style sheet"
+    },
+  
+    {
+      title: "What is the correct way to close a <tag>?",
+      multiChoice: ["<close>", "(close)", "<close/>", "</close>"],
+      answer: "</close>"
+    },
+  
+    {
+      title: "If code was a class in html how would you target it in css?",
+      multiChoice: ["code", ".code", "code.", "./code"],
+      answer: ".code"
+    },
+  
+    {
+      title: "Do you need a closing doctype html tag?",
+      multiChoice: ["Yes", "No"],
+      answer: "No"
+    },
+  
+    {
+      title: "What makes the webpage have animations?",
+      multiChoice: ["html", "css", "javascript"],
+      answer: "javascript"
+    }
+  ];
